24bfe0d4502fa2827159c4e4923e004a4e7c403c
DAOUDI Soufian <soufian.daoudi2@gmail.com>
2019-03-02 12:20:18 +0000

    Update docker entrypoint script

--- php56/docker-entrypoint.sh
+++ php56/docker-entrypoint.sh
@@ -3,13 +3,21 @@
 uid=$(stat -c %u /var/www)
 gid=$(stat -c %g /var/www)
 
-sed -i -r "s/www-data:x:\d+:\d+:/www-data:x:$uid:$gid:/g" /etc/passwd
-sed -i -r "s/www-data:x:\d+:/www-data:x:$gid:/g" /etc/group
-chown $uid:$gid /var/www
-
-user=$(grep ":x:$uid:" /etc/passwd | cut -d: -f1)
-if [ $# -eq 0 ]; then
-    composer
+if [ $uid == 0 ] && [ $gid == 0 ]; then
+    if [ $# -eq 0 ]; then
+        composer
+    else
+        exec "$@"
+    fi
 else
-    exec su-exec $user "$@"
+    sed -i -r "s/www-data:x:\d+:\d+:/www-data:x:$uid:$gid:/g" /etc/passwd
+    sed -i -r "s/www-data:x:\d+:/www-data:x:$gid:/g" /etc/group
+    chown $uid:$gid /var/www
+
+    user=$(grep ":x:$uid:" /etc/passwd | cut -d: -f1)
+    if [ $# -eq 0 ]; then
+        composer
+    else
+        exec su-exec $user "$@"
+    fi
 fi
